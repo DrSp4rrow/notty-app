7f98fdb2f7b72f2c8f6b4092a1083672dd39ccf2
DrSp4rrow <frankantony17@hotmail.com>
2025-02-02 14:31:46 -0500

    eliminando

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authMiddleware = require('../middlewares/authMiddleware');
-const { getUser } = require('../controllers/userController');
-
-const router = express.Router();
-
-router.get('/profile', authMiddleware, getUser);
-
-module.exports = router;
